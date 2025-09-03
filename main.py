eb8b1f7c21ebb5bcf7c9b3ef2145b065b1f48aad
RahJess <jessicakantotiana@gmail.com>
2025-09-03 09:30:41 +0200

    add: finish exo1 - d

--- main.py
+++ main.py
@@ -37,6 +37,20 @@ def create_phones(new_phone: List[Phone]):
     phones_db.extend(created_phone)
     return created_phone
 
+# EXO1 - c
+@app.get("/posts", response_model=List[Phone], status_code=status.HTTP_200_OK)
+def get_phones():
+    return phones_db
+
+# EXO1 - d
+@app.get("/phones/{phone_id}", response_model=Post, status_code=status.HTTP_200_OK)
+def read_phone_by_id(phone_id: int):
+    for i, existing_post in enumerate(phones_db):
+        if existing_post.id == phone_id:
+            return existing_post
+        else:
+            return Response(status_code=status.HTTP_404_NOT_FOUND)
+
 # Q2
 @app.get("/home", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
 def welcome_home():
@@ -54,23 +68,3 @@ def welcome_home():
     """
 
 
-
-# Q5 - Récupérer tous les posts
-@app.get("/posts", response_model=List[Phone], status_code=status.HTTP_200_OK)
-def get_phones():
-    return phones_db
-
-
-# Q6 - Mettre à jour un post par ID
-@app.put("/posts/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
-def update_post(post_id: int, new_post: Phone):
-    for i, existing_post in enumerate(phones_db):
-        if existing_post.id == post_id:
-            updated_post = Post(id=post_id, **new_post.model_dump())
-            phones_db[i] = updated_post
-            return updated_post
-
-    # Si le post n'existe pas encore, on l'ajoute
-    new_post_with_id = Post(id=post_id, **new_post.model_dump())
-    phones_db.append(new_post_with_id)
-    return new_post_with_id
